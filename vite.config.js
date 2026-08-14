2c4e1fd800b7e4d7b8d84ec6962351a9d4a9a504
sanyi246751 <sanyi246751@gmail.com>
2026-08-14 11:57:20 +0800

    修正標籤列印時畫面空白的問題

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 測試 Git 自動偵測變更功能
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Miaoli/',
